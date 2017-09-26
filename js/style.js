2a36428499f501e797a6648cb0211ccd6f47b625
Zay19 <31999291+Zay19@users.noreply.github.com>
2017-09-26 09:03:30 -0500

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,15 @@
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+    
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
