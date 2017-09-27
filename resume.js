2d7b9fb9747551f1d60a9d3314200d20851d059e
Zay19 <31999291+Zay19@users.noreply.github.com>
2017-09-27 09:05:37 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,54 @@
+
+
+var str1 = "ZaShonne Bryant-Shaw.";  
+var str2 = str1.toUpperCase();  
+document.write(str2);
+
+
+function displayPosition(companyName1, jobTitle1, description1, companyName2, jobTitle2, description2) {
+    console.log(companyName1 + jobTitle1, description1)
+    console.log(companyName2 + jobTitle2, description2)
+
+
+displayPosition('*' + companyName1 + jobTitle1, description1)
+displayPosition('*' + companyName2 + jobTitle2, description2)
+
+var companyName1= 'StoreHouse-SelfStorage'
+var companyName2= 'Waffle House'
+
+var jobTitle1= 'Maintenance'
+var jobTitle2= 'Grill-operator'
+
+var description1='helped customers move in/out of their storages; keept grounds and storages clean'
+var description2='cooked food'
+};
+
+
+
+
+
+ 
+
+var myCareer = 'Astronaut';
+var myDescription = 'I am a 19 year old African-American. Graduated in May 2016. I only attended college for 1 year and immediatly left. Also I have dreads.'
+var myInterest = 'Video games, Skating, Anime, Music, Drawing';
+
+var mySkills = 'Check it: coding; Drawing; Check it: handling money; Command prompt'
+
+
+
+
+
+
+
+
+
+
+console.log('Name:' + str2)
+
+console.log('Career field:' + myCareer)
+console.log('Description:' + myDescription)
+console.log('My Interests:' + myInterest)
+console.log('Past Positions:' + myPastPositions)
+console.log('My Skills:' + mySkills)
+
