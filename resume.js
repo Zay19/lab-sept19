2d49d395a027c56d3b18547d6ef59e87b57e726b
Zay19 <31999291+Zay19@users.noreply.github.com>
2017-09-27 09:35:33 -0500

    Add files via upload
    reupload

--- resume.js
+++ resume.js
@@ -3,25 +3,28 @@
 var str1 = "ZaShonne Bryant-Shaw.";  
 var str2 = str1.toUpperCase();  
 document.write(str2);
+console.log('Name:' + str2)
+
+var myCareer = 'Astronaut';
+console.log('Career field:' + myCareer)
 
 
-function displayPosition(companyName1, jobTitle1, description1, companyName2, jobTitle2, description2) {
-    console.log(companyName1 + jobTitle1, description1)
-    console.log(companyName2 + jobTitle2, description2)
 
-
-displayPosition('*' + companyName1 + jobTitle1, description1)
-displayPosition('*' + companyName2 + jobTitle2, description2)
-
-var companyName1= 'StoreHouse-SelfStorage'
-var companyName2= 'Waffle House'
-
-var jobTitle1= 'Maintenance'
-var jobTitle2= 'Grill-operator'
-
-var description1='helped customers move in/out of their storages; keept grounds and storages clean'
-var description2='cooked food'
+function displayPosition(myPast, companyName1, jobTitle1, description1, companyName2, jobTitle2, description2) {
+   displayPosition(myPast)
+displayPosition( companyName1 + jobTitle1, description1)
+displayPosition( companyName2 + jobTitle2, description2)
 };
+
+console.log(myPast= 'My Past Experience:')
+console.log(companyName1= '*' + 'StoreHouse-SelfStorage')
+console.log(jobTitle1= '-' + 'Maintenance')
+console.log(description1='-' + 'helped customers move in/out of their storages; keept grounds and storages clean')
+
+console.log(companyName2= '*' + 'Waffle House')
+console.log(jobTitle2= '-' + 'Grill-operator')
+console.log(description2='-' + 'cooked food')
+
 
 
 
@@ -29,7 +32,7 @@ var description2='cooked food'
 
  
 
-var myCareer = 'Astronaut';
+
 var myDescription = 'I am a 19 year old African-American. Graduated in May 2016. I only attended college for 1 year and immediatly left. Also I have dreads.'
 var myInterest = 'Video games, Skating, Anime, Music, Drawing';
 
@@ -44,11 +47,11 @@ var mySkills = 'Check it: coding; Drawing; Check it: handling money; Command pro
 
 
 
-console.log('Name:' + str2)
 
-console.log('Career field:' + myCareer)
+
+
 console.log('Description:' + myDescription)
 console.log('My Interests:' + myInterest)
-console.log('Past Positions:' + myPastPositions)
+
 console.log('My Skills:' + mySkills)
 
