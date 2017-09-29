ecdfc0218c77b16e9c2f5c92b02583ddf727d8f3
Zay19 <31999291+Zay19@users.noreply.github.com>
2017-09-29 09:07:29 -0500

    Add files via upload

---
+++ stylept2.js
@@ -0,0 +1,122 @@
+var friends= ['Pj', 'Amon', 'Aj', 'Rico', 'Trey'];
+
+btn.addEventListener('click',function myFunction(){
+var btn = document.createElement("BUTTON");            
+document.body.appendChild(btn);                    
+})
+
+
+btn.addEventListener('click', function myFunction(){
+var div = document.createElement("div");
+div.className("friend")
+
+var heada= document.createElement("h3")
+var t= document.createTextNode("Pj, Trey, Amon, Rico, Aj")
+heada.appendChild(t)
+document.body.appendChild(heada)
+
+var para= document.createElement("p")
+var e= document.createTextNode("lines of code in the file")
+var f= document.createTextNode("lines of code")
+var g= document.createTextNode("strikes one out, clears it all out")
+var h= document.createTextNode("lines of code in the file")
+para.appendChild(e, f, g, h)
+document.body.appendChild(para);
+
+
+
+var div = document.createElement("div");
+div.className("friend")
+
+var para= document.createElement("p")
+var e= document.createTextNode("lines of code in the file")
+var f= document.createTextNode("lines of code")
+var g= document.createTextNode("strikes one out, clears it all out")
+var h= document.createTextNode("lines of code in the file")
+para.appendChild(e, f, g, h)
+document.body.appendChild(para);
+
+var div = document.createElement("div");
+div.className("friend")
+
+var para= document.createElement("p")
+var e= document.createTextNode("lines of code in the file")
+var f= document.createTextNode("lines of code")
+var g= document.createTextNode("strikes one out, clears it all out")
+var h= document.createTextNode("lines of code in the file")
+para.appendChild(e, f, g, h)
+document.body.appendChild(para);
+
+
+var div = document.createElement("div");
+div.className("friend")
+
+var para= document.createElement("p")
+var e= document.createTextNode("lines of code in the file")
+var f= document.createTextNode("lines of code")
+var g= document.createTextNode("strikes one out, clears it all out")
+var h= document.createTextNode("lines of code in the file")
+para.appendChild(e, f, g, h)
+document.body.appendChild(para);
+
+var div = document.createElement("div");
+div.className("friend")
+
+var para= document.createElement("p")
+var e= document.createTextNode("lines of code in the file")
+var f= document.createTextNode("lines of code")
+var g= document.createTextNode("strikes one out, clears it all out")
+var h= document.createTextNode("lines of code in the file")
+para.appendChild(e, f, g, h)
+document.body.appendChild(para);
+
+var div = document.createElement("div");
+div.className("friend")
+
+var para= document.createElement("p")
+var e= document.createTextNode("lines of code in the file")
+var f= document.createTextNode("lines of code")
+var g= document.createTextNode("strikes one out, clears it all out")
+var h= document.createTextNode("lines of code in the file")
+para.appendChild(e, f, g, h)
+document.body.appendChild(para);
+})
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function (){
+
+ 
+ myFunction();
+
+}
+   
+    
+    
+
+
+
+    for (var i = 0; i < friends.length; i++) {
+    console.log(friends[i]),(':' );
+    
+    for(j=99; j > 1; j--) {
+        console.log(j + ' lines of code in the file,' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' lines of code in the file');
+    }     
+     if(j===1) {
+        console.log(j + ' line of code in the file, ' + j + ' line of code, ' + friends[i] + ' strike ones out, clears it all out,' + (j-1) +' no more lines of code in the file');
+    }
+
+}
+
+
+
+
+
